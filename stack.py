3fd357763c53b62aa7ae816f22abb726f0b6e121
Yash-3122 <96653022+Yash-3122@users.noreply.github.com>
2022-04-17 00:13:11 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,23 @@
+## 20CE112 Yash Patel
+## Github Repository Link
+## https://github.com/Yash-3122/Assignment8
+## Practical 8
+
+
+
+STACK = []
+
+STACK.append('A')
+STACK.append('B')
+STACK.append('C')
+
+print('INITIAL STACK')
+print(STACK)
+
+print('\nELEMENTS POPPED FROM STACK:')
+print(STACK.pop())
+print(STACK.pop())
+print(STACK.pop())
+
+print('\nSTACK AFTER ELEMENTS ARE POPPED:')
+print(STACK)
